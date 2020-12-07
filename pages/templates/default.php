ca255da3f315e007949778153bb406799600fd3d
AzCraft <azcraft1001@gmail.com>
2020-12-07 16:38:54 +0200

    Design update.

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -9,7 +9,9 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title><?php echo $dictionary->title; ?></title>
+        <title>
+		<?php echo $dictionary->title; ?>
+		</title>
 
         <link href="stylesheets/main.css"
               rel="stylesheet" type="text/css">
@@ -19,7 +21,18 @@
               rel="stylesheet" type="text/css">
     </head>
     <body>
-        <?php require "pages/templates/default_navigation.php"; ?>
+		<?php require "pages/templates/default_navigation.php"; ?>
         <?php include "pages/" . $page . ".php"; ?>
+		<br/>
+		<br/>
+		<div id="nachalo">
+			<div id="page">
+				<div id="formuli">
+					Формули:
+				</div>
+				Условие:   
+				<input id="uslovie" placeholder="в задачата, чрез влачене се добавят стойности/действия" type="text">
+			</div>
+		</div>
     </body>
 </html>
--- pages/templates/default_navigation.php
+++ pages/templates/default_navigation.php
@@ -5,7 +5,19 @@
 ?>
 
 <div id="navigation_container">
-
-
-
+	<div class="button">
+		<a href="">
+			Задачи
+		</a>
+	</div>
+	<div class="button">
+		<a href="">
+			Тестове
+		</a>
+	</div>
+	<div class="button">
+		<a href="">
+			Резултати
+		</a>
+	</div>
 </div>
