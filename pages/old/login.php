d69b4b716d8a55ccba68c69f8a4f5ba5fc130033
Elfa <fakemail@mymail.bg>
2020-12-22 05:45:16 +0200

    Added Home page.

--- pages/old/login.php
+++ pages/old/login.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		
-		<link rel="stylesheet" href="./stylesheets/login.css" type="text/css">
-	</head>
-		<div id="page">
-	<body>
+    <head>
+
+        <link rel="stylesheet" href="./stylesheets/login.css" type="text/css">
+    </head>
+    <div id="page">
+        <body>
             <div id="page">
                 <div id="menu">
-		<div id="home">
-			<a href="">
-				Вход
-			</a>
-		</div>
-		<div id="register">
-			<a href="">
-				Регистрация
-			</a>
-		</div>
+                    <div id="register">
+                        <a href="">
+                            Регистрация
+                        </a>
+                    </div>
+                    <div id="home">
+                        <a href="">
+                            Вход
+                        </a>
+                    </div>
                 </div>
-			<div id="start">
-				<div id="logo">
-					
-				</div>
-				<form action="/action_page.php">
-					<br/>
-					<input class="password" placeholder="Име" type="text">
-					<br/>
-					<input class="password" placeholder="Парола" type="password">
-				</form> 
-			</div>
+                <div id="start">
+                    <div id="logo">
+
+                    </div>
+                    <form action="/action_page.php">
+                        <br/>
+                        <input class="password" placeholder="Име" type="text">
+                        <br/>
+                        <input class="password" placeholder="Парола" type="password">
+                    </form> 
+                </div>
             </div>
-	</body>
+        </body>
 </html>
