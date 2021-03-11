1cd8734ece675d9f595f64918e94a03ce34070da
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2021-03-11 11:59:58 +0200

    Added comments to includePage.php

--- include/scripts/authorizeUserBySession.php
+++ include/scripts/authorizeUserBySession.php
@@ -9,13 +9,15 @@
 require "include/session.php";
 require "include/user.php";
 
+use Identification\User;
+
 /**
  * @global null|Identification\User $user
  */
 if (!defined("USER_AUTHORIZED")){
     if ($session->user !== null){
         define("USER_AUTHORIZED", true);
-        $user = Identification\User::fromSession($db, $session);
+        $user = User::fromSession($db, $session);
     }
 }
 
--- include/scripts/includePage.php
+++ include/scripts/includePage.php
@@ -6,29 +6,36 @@
  * and open the template in the editor.
  */
 
+include "include/dictionary.php";
 
 if (!defined("DETECTED_PAGE")){
     define("DETECTED_PAGE", true);
 
+    /* Defaults */
     $page = "home";
     $pageType = null;
+
+    /* Input */
     if (isset($_GET["test"]) && is_string($_GET["test"])){
         $page = "joinTest";
     } else if (isset($_GET['p']) && is_string($_GET['p'])){
-        if (preg_match("/^[a-zA-Z0-9\_]{2,}$/", $_GET['p'])){
+        if (preg_match("/^[a-zA-Z0-9\_]{2,20}$/", $_GET['p'])){
             $page = $_GET['p'];
         }
     }
 
-    $dirs = ["content", "standalone", "ajax", "test", "files"];
-
+    /* Validation */
+    $dirs = ["content", "standalone", "ajax", "test", "file", "sse"];
     foreach ($dirs as $dir){
+        if (!is_dir("pages/" . $dir)){
+            continue;
+        }
         if (in_array($page . ".php", scandir("pages/" . $dir))){
             $pageType = $dir;
             break;
         }
     }
-    unset($dirs);
+    unset($dirs, $dir);
 
     if (!isset($pageType)){
         $pageType = "standalone";
@@ -36,15 +43,17 @@ if (!defined("DETECTED_PAGE")){
     }
 }
 
-if (!defined("CHECKED_ACCESS_PERMISSIONS") && defined("USER_AUTHORIZED")){
+if (!defined("CHECKED_ACCESS_PERMISSIONS") && defined("USER_AUTHORIZED") && defined("DETECTED_PAGE")){
     define("CHECKED_ACCESS_PERMISSIONS", true);
 
+    /* Default values */
     $defaultGuestPage = "home";
     $userOnlyPages = [];
 
     $defaultUserPage = "tests";
     $guestOnlyPages = [];
 
+    /* Config values */
     if (file_exists("data/accessPermissions.json")){
         $raw = file_get_contents("data/accessPermissions.json");
         $data = json_decode($raw, 1);
@@ -57,65 +66,82 @@ if (!defined("CHECKED_ACCESS_PERMISSIONS") && defined("USER_AUTHORIZED")){
         unset($raw, $data);
     }
 
-
+    /* Detect Forward Location */
     $forwardTo = null;
-
     if (in_array($page, $userOnlyPages) && !isset($user)){
         $forwardTo = $defaultGuestPage;
     } else if (in_array($page, $guestOnlyPages) && isset($user)){
         $forwardTo = $defaultUserPage;
     }
+    unset($defaultGuestPage, $userOnlyPages, $defaultUserPage, $guestOnlyPages);
 
-    if (isset($forwardTo)){
-        if ($pageType === "ajax"){
-            echo json_encode([
-                "msg" => $dictionary->forbidden,
-                "code" => "Forbidden",
-                "reload" => false
-            ]);
-            unset($response);
-
-            $page = null;
-            $pageType = null;
-        } else {
-            $page = $forwardTo;
-            $pageType = "standalone";
-        }
+    /* Custom operations */
+    if (isset($forwardTo) && $pageType === "ajax"){
+        header("HTTP/1.1 403 Forbidden", true, 403);
+        echo json_encode([
+            "msg" => $dictionary->forbidden,
+            "code" => "Forbidden",
+            "reload" => false
+        ]);
+        unset($page, $pageType, $forwardTo);
+        return;
     }
 
-    unset($defaultGuestPage, $userOnlyPages, $defaultUserPage, $guestOnlyPages, $forwardTo);
+    /* Default operation */
+    if (isset($forwardTo)){
+        $page = $forwardTo;
+        $pageType = "standalone";
+    }
+    unset($forwardTo);
 }
 
 if (!defined("INCLUDED_PAGE")){
     define("INCLUDED_PAGE", true);
 
     switch ($pageType){
-        case "file":
-            include "pages/files/" . $page . ".php";
+        /* Default actions */
+        default: /* Not Found */
+            header("HTTP/1.1 404 Not Found", true, 404);
             break;
 
+        case "file": /* Custom file transfers (To Do) */
+        case "test": /* Test Files */
+        case "standalone": /* Normal HTML (To Do) */
+        case "sse": /* SSE API (In Progress) */
+            include "pages/" . $pageType . "/" . $page . ".php";
+            break;
+
+        /* Template HTML (To Do) */
+        case "content":
+            include "pages/templates/default.php";
+            break;
+
+        /* AJAX API */
         case "ajax":
+            /* Init */
+            $input = null;
             $method = 'get';
             $contentType = null;
-            if (is_string($_SERVER['REQUEST_METHOD'])){
-                $method = $_SERVER['REQUEST_METHOD'];
-                $contentType = trim($_SERVER["CONTENT_TYPE"] ?? '');
-            }
-            $input = null;
-
-            if ($method === 'POST' && $contentType === 'application/json'){
-                $inputRaw = file_get_contents("php://input");
-                $input = json_decode($inputRaw, true);
-            }
-
             $response = [
                 "msg" => $dictionary->formMessages["noAction"],
                 "code" => null,
                 "reload" => false
             ];
-            $success = include "pages/ajax/" . $page . ".php";
 
+            /* Input */
+            if (is_string($_SERVER['REQUEST_METHOD'])){
+                $method = $_SERVER['REQUEST_METHOD'];
+                $contentType = trim($_SERVER["CONTENT_TYPE"] ?? '');
+            }
+            if ($method === 'POST' && $contentType === 'application/json'){
+                $inputRaw = file_get_contents("php://input");
+                $input = json_decode($inputRaw, true);
+            }
+
+            /* Action */
+            $success = include "pages/ajax/" . $page . ".php";
             if ($success === false){
+                header("HTTP/1.1 403 Forbidden", true, 403);
                 echo json_encode([
                     "msg" => $dictionary->forbidden,
                     "code" => "Forbidden",
@@ -123,21 +149,13 @@ if (!defined("INCLUDED_PAGE")){
                 ]);
                 break;
             }
+
+            /* Output */
             echo json_encode($response);
-            unset($response);
-            break;
-
-        case "test":
-            include "pages/test/" . $page . ".php";
-            break;
-
-        case "content":
-            include "pages/templates/default.php";
-            break;
-
-        case "standalone":
-            include "pages/standalone/" . $page . ".php";
+            unset($method, $contentType);
+            unset($input, $inputRaw);
+            unset($success, $response);
             break;
     }
     unset($pageType, $page);
-}
+}  
