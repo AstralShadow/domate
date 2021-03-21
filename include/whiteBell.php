c7ab03385c03163583df96f6ee6b6c24fd8e9161
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2021-03-21 10:44:05 +0200

    Created include file for WhiteBell.

---
+++ include/whiteBell.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * This file defines $whitebell variable based on socket location.
+ * WhiteBell is an event-transfer-server software.
+ * Very usefyl in Server-Send-Events based web applications.
+ */
+
+include "submodules/WhiteBellPHPClient/WhiteBellClient.php";
+
+use WhiteBell\WhiteBellClient;
+
+$whiteBellLocation = trim(file_get_contents("data/private/whitebell.location"));
+
+if (!file_exists($whiteBellFile)){
+    unset($whiteBellFile);
+    $whitebell = null;
+    return;
+}
+
+$whitebell = new WhiteBellClient($whiteBellFile, "DomaTe");
+unset($whiteBellFile);
